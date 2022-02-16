a4e11179370e993c885260ac102e54fb91f85ae2
Karin Wan <karinwan@v1040-wn-rt-a-50-89.campus-dynamic.uwaterloo.ca>
2022-02-16 15:21:06 -0500

    Create new activity FolderStructureActivity

---
+++ Note/app/src/main/java/com/example/note/activities/FolderStructure.kt
@@ -0,0 +1,12 @@
+package com.example.note.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.note.R
+
+class FolderStructure : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_folder_structure)
+    }
+}
